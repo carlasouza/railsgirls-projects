7504d2f451bbc63591079d46360eb2b33867a1a1
Carla Souza <contact@carlasouza.com>
2013-04-27 17:07:55 -0300

    Cosmetics

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130427181815) do
+ActiveRecord::Schema.define(:version => 20130427160519) do
 
   create_table "authors", :force => true do |t|
     t.string   "nome"
@@ -33,7 +33,6 @@ ActiveRecord::Schema.define(:version => 20130427181815) do
     t.string   "country"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-    t.string   "name"
   end
 
   create_table "ideas", :force => true do |t|
